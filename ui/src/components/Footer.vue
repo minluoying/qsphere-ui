f4c8b8522c7b15f3c9d78798d153b54e746b6a3b
Will <seoktaehyeon@msn.com>
2020-06-08 20:05:35 +0800

    update code in github link

--- ui/src/components/Footer.vue
+++ ui/src/components/Footer.vue
@@ -2,6 +2,6 @@
   <div>
     <el-link href="https://QualitySphere.github.io" target="_blank" type="info" style="margin: 15px;">Homepage</el-link>
     |
-    <el-link href="https://github.com/QualitySphere/qsphere" target="_blank" type="info" style="margin: 15px;">Code in GitHub</el-link>
+    <el-link href="https://github.com/QualitySphere" target="_blank" type="info" style="margin: 15px;">Code in GitHub</el-link>
   </div>
 </template>
