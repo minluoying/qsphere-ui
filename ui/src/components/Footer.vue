86e98b573a164e405dbb61703fc8c7c6eee3f7d8
seoktaehyeon <seoktaehyeon@msn.com>
2021-01-20 10:34:40 +0800

    change footer

--- ui/src/components/Footer.vue
+++ ui/src/components/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
-    <el-link href="https://QualitySphere.github.io" target="_blank" type="info" style="margin: 15px;">Homepage</el-link>
+    <el-link href="https://QualitySphere.github.io" target="_blank" type="info" style="margin: 15px;">Website</el-link>
     |
-    <el-link href="https://github.com/QualitySphere" target="_blank" type="info" style="margin: 15px;">Code in GitHub</el-link>
+    <el-link href="https://github.com/QualitySphere" target="_blank" type="info" style="margin: 15px;">GitHub</el-link>
   </div>
 </template>
